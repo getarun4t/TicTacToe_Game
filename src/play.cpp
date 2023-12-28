dcccba622daa74e08674e0369eecabfb3be81584
Arun Sasi <aruntdpa@gmail.com>
2023-12-28 13:06:04 +0100

    Committing some changes

--- src/play.cpp
+++ src/play.cpp
@@ -1,17 +1,15 @@
 #include "\include\play.hpp"
 
-Play::getNextOption() {
-	pair<int, int> input;
+void Play::getNextOption() {
 	do {
 		cout << "Enter the x co-ordinates" << endl;
-		cin >> input.first;
+		cin >> nextOption.first;
 		cout << "Enter the y co-ordinates" << endl;
-		cin >> input.second;
-	} while (Play::isValid(input.first) && Play::isValid(input.second));
-	return input;
+		cin >> nextOption.second;
+	} while (Play::isValid(nextOption.first) && Play::isValid(nextOption.second));
 }
 
-Play::isValid(int coordinate) {
+bool Play::isValid(int coordinate) {
 	if (Play::board[coordinate] == 0 && coordinate < 3) {
 		Play::board[coordinate] == 1;
 	}
@@ -22,12 +20,17 @@ Play::isValid(int coordinate) {
 	return true;
 }
 
-Play::printBoard(Play::board) {
+void Play::printBoard() {
+	cout << endl;
+	cout << "Current board is below" << endl;
 	for (auto y : board) {
 		for (auto x : y) {
 			cout << "| ";
 			if (x) {
-				cout << x ;
+				if (x == 1)
+					cout << "X";
+				else
+					cout << "O";
 			}
 			else {
 				cout << " ";
@@ -40,6 +43,40 @@ Play::printBoard(Play::board) {
 	}
 }
 
-Play::checkGameEnd() {
+void Play::MarkInBoard() {
+	if (A.getTurn)
+		board[nextOption.first][nextOption.second] == 1;
+	else
+		board[nextOption.first][nextOption.second] == 2;
+}
 
+void Play::switchPlayer() {
+	if (!A.getTurn) {
+		A.setTurn();
+		B.resetTurn();
+	}
+	else {
+		B.setTurn();
+		A.resetTurn();
+	}
+}
+
+Play::Play() {
+	checkedCellCount = 0;
+	board = vector<vector<int>>(3, vector<int>(3, 0));
+}
+
+void Play::run() {
+	do {
+		switchPlayer();
+		printBoard();
+		getNextoption();
+		MarkInBoard();
+	} while (checkGameEnd());
+}
+
+bool Play::checkGameEnd() {
+	if (checkedCellCount != 9) {
+		
+	}
 }
