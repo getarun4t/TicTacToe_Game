5db5c09de1df3e9bddef046478d8187dcd23c9b9
Arun Sasi <aruntdpa@gmail.com>
2024-01-15 20:08:36 +0100

    Sample file commit

--- tests/MyTests.cpp
+++ tests/MyTests.cpp
@@ -1,5 +1,13 @@
 #include <gtest/gtest.h>
+#include "play.hpp"
 
-TEST(MyTestSuite, MyTestCase) {
-    ASSERT_EQ(2 + 2, 4);
+TEST(Play, checkGameEnd) {
+	Play game;
+
+	EXPECT_EQ(game.checkGameEnd(), true);
+
+	game.board[0][0] = "X";
+	game.board[1][1] = "X";
+	game.board[2][2] = "X";
+	EXPECT_EQ(game.checkGameEnd(), false);
 }
