947a1bac6aab617c950dd775e0dc15c3a8f7bf6a
Arun Sasi <38099892+getarun4t@users.noreply.github.com>
2024-01-17 21:32:19 +0100

    Creating working test file (#34)
    * Sample file commit
    
    * Adding a test file
    
    * Updating CMake
    
    * Updating the test file

--- tests/MyTests.cpp
+++ tests/MyTests.cpp
@@ -1,6 +1,18 @@
 #include <gtest/gtest.h>
+#include "../include/play.hpp"
 
 TEST(Play, Demo) {
 	int demo_int = 2;
 	ASSERT_EQ(demo_int, 2);
+}
+
+TEST(Play, TestFile) {
+  Play game;
+
+  EXPECT_EQ(game.checkGameEnd(), true);
+
+  game.board[0][0] = "X";
+  game.board[1][1] = "X";
+  game.board[2][2] = "X";
+  EXPECT_EQ(game.checkGameEnd(), false);
 }
