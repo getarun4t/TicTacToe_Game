0400370a1655d7e74bc0d2ce2480960c9ae50be6
Arun Sasi <38099892+getarun4t@users.noreply.github.com>
2024-01-24 10:21:32 +0100

    Removing demo tests (#43)
    * Adding a positive test
    
    * Removing demo test and renaming

--- tests/MyTests.cpp
+++ tests/MyTests.cpp
@@ -1,12 +1,7 @@
 #include <gtest/gtest.h>
 #include "../include/play.hpp"
 
-TEST(Play, Demo) {
-	int demo_int = 2;
-	ASSERT_EQ(demo_int, 2);
-}
-
-TEST(Play, TestFile) {
+TEST(Play, SanityTest) {
   Play game;
   EXPECT_EQ(game.checkGameEnd(), true);
 }
