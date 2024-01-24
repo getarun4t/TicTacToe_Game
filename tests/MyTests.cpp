15226528304108412e7163d3ff974789a463bde7
Arun Sasi <aruntdpa@gmail.com>
2024-01-24 10:17:18 +0100

    Removing demo test and renaming

--- tests/MyTests.cpp
+++ tests/MyTests.cpp
@@ -1,20 +1,7 @@
 #include <gtest/gtest.h>
 #include "../include/play.hpp"
 
-TEST(Play, Demo) {
-	int demo_int = 2;
-	ASSERT_EQ(demo_int, 2);
-}
-
-TEST(Play, TestFile) {
+TEST(Play, SanityTest) {
   Play game;
   EXPECT_EQ(game.checkGameEnd(), true);
-}
-
-TEST(Play, PositiveTest) {
-	Play game;
-	game.setBoardValue(0, 0, "X");
-	game.setBoardValue(1, 1, "X");
-	game.setBoardValue(2, 2, "X");
-	EXPECT_EQ(game.checkGameEnd(), false);
 }
